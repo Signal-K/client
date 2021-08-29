e7bcc4029176cd027e2ba1c002adc905d2d1de11
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-08-29 12:56:28 +0000

    GSCA-11 Add Phaser // Moralis Config for test/demo

---
+++ phaser/main.js
@@ -0,0 +1,61 @@
+var BootScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+
+    preload: function ()
+    {
+        // Load the resources here
+    },
+
+    create: function ()
+    {
+        this.scene.start('WorldScene');
+    }
+});
+
+var WorldScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function WorldScene ()
+    {
+        Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+    preload: function ()
+    {
+        // Load the resources here for world scene
+    },
+    create: function ()
+    {
+        // create your world here
+    }
+});
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 320,
+    heigh: 240,
+    zoom: 2,
+    pixelArt: true, // prevents blue of the textures when scaled
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene
+    ]
+};
+var game = new Phaser.Game(config);
