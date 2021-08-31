ebd229770c6ecc85151dc7f42ea8f9597fdc791a
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-08-31 05:43:14 +0000

    GSCA-11 Phaser Add Arcade physics for character movement

--- phaser/main.js
+++ phaser/main.js
@@ -12,6 +12,15 @@ var BootScene = new Phaser.Class({
     preload: function ()
     {
         // Load the resources here
+
+        // Map tiles
+        this.load.image('tiles', 'assets/map/spritesheet.png');
+
+        // map in json format
+        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+
+        // The two characters
+        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16});
     },
 
     create: function ()
@@ -33,13 +42,51 @@ var WorldScene = new Phaser.Class({
     preload: function ()
     {
         // Load the resources here for world scene
-
-        // Map tiles
-        this.load.image('tiles', 'assets/map/spritesheet.png');
     },
     create: function ()
     {
         // create your world here
+        var map = this.make.tilemap({ key: 'map' }); // Create world scene with map loaded in preload function of BootScene
+
+        // Load layers of the map
+        var tiles = map.addTilesetImage('spritesheet', 'tiles'); // Creates tileset image
+        var grass = map.createStaticLayer('Grass', tiles, 0, 0); // Adds grass layer to the map
+            var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0); // Adds obstacle layer to the map
+            obstacles.setCollisionByExclusion([-1]); 
+
+        // Add player/character sprite
+        this.player = this.physics.add.sprite(50, 100, 'player', 6); // Parameters (in order): x,y coord, image resource, frame (of image resource/character/object)
+
+        // Using Phaser.io 3 Arcade Physics to move around on the world map
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        this.player.setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    },
+    update: function (time, delta)
+    { // Set body velocity of the sprite/character object according to the direction we want to move the character
+        this.player.body.setVelocity(0);
+
+            // Horizontal movement
+            if (this.cursors.left.isDown)
+            {
+                this.player.body.setVelocityX(-80);
+            }
+            else if (this.cursors.right.isDown)
+            {
+                this.player.body.setVelocityX(80);
+            }
+
+            // Vertical movement
+            if (this.cursors.up.isDown)
+            {
+                this.player.body.setVelocityY(-80);
+            }
+            else if (this.cursors.down.isDown)
+            {
+                this.player.body.setVelocityY(80);
+            }
     }
 });
 
