3d2eea741c92b0f937a5cc0627f1717f75ddb34d
Gizmotronn <droidology@allianceofdroids.org.au>
2021-09-28 17:07:05 +0800

    Assets for GSCA-9

---
+++ phaser/turnbased-combat/start.js
@@ -0,0 +1,22 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 320,
+    height: 240,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false // set to true to view zones
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene,
+        BattleScene,
+        UIScene
+    ]
+};
+var game = new Phaser.Game(config);
