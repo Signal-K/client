7069e3c9ab93c8ade749c16b9db92017d0bf7ad7
Gizmotronn <droidology@allianceofdroids.org.au>
2021-10-03 16:14:37 +0800

    🔑 GSCA-9 Phaser Add config file

---
+++ js/game.js
@@ -0,0 +1,23 @@
+const { Phaser } = require("./phaser");
+
+var config = {
+    type: Phaser.WEBGL,
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { x: 0, y: 0 }
+        }
+    },
+    scene: [
+        SceneMainMenu,
+        SceneMain,
+        SceneGameOver
+    ],
+    pixelArt: true,
+    roundPixels: true
+};
+
+var game = new Phaser.Game(config);
