7332f9a54601e3de3877756e56e674498403bc0e
Liam Arbuckle <liam@skinetics.tech>
2022-09-26 20:50:03 +0800

    🎋🥬 ↝ [SGV2-22]: I have a constant fear that someone's always near

--- components/_Core/Section/Planet.tsx
+++ components/_Core/Section/Planet.tsx
@@ -28,13 +28,13 @@ const Planet: React.FC<PlanetProps> = ({
   centerButtonImage,
 }) => {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex overflow-hidden relative flex-col w-full min-h-[2413px] max-md:max-w-full">
-        <img
-          loading="lazy"
-          src={backgroundImage}
-          className="object-cover absolute inset-0 size-full"
-        />
+    <div className="flex flex-col justify-center overflow-hidden h-screen">
+      <img
+        loading="lazy"
+        src={backgroundImage}
+        className="object-cover absolute inset-0 z-0"
+      />
+      <div className="relative z-10 flex-1">
         {/* <Header
           logoImage={logoImage}
           planetName={planetName}
@@ -42,12 +42,14 @@ const Planet: React.FC<PlanetProps> = ({
           rightArrowImage={rightArrowImage}
           compassImage={compassImage}
         /> */}
-        <RoverSection
-          structureImage={structureImage}
-          activeRoverImage={activeRoverImage}
-          idleRoverImage={idleRoverImage}
-        />
-        <Gallery images={galleryImages} centerButtonImage={centerButtonImage} />
+        <div className="flex flex-col justify-between h-full pb-8">
+          <RoverSection
+            structureImage={structureImage}
+            activeRoverImage={activeRoverImage}
+            idleRoverImage={idleRoverImage}
+          />
+          <Gallery images={galleryImages} centerButtonImage={centerButtonImage} />
+        </div>
       </div>
     </div>
   );
@@ -68,42 +70,19 @@ const Header: React.FC<HeaderProps> = ({
   rightArrowImage,
   compassImage,
 }) => {
-    return (
-        <div className="flex relative flex-col justify-center w-full backdrop-blur-[5px] bg-white bg-opacity-0 max-md:max-w-full">
-          <div className="flex flex-col justify-center items-center px-12 py-5 w-full max-md:px-5 max-md:max-w-full md:flex-row md:justify-between">
-            <div className="flex flex-col justify-center items-center md:flex-row md:items-center">
-              <img
-                loading="lazy"
-                src={logoImage}
-                className="shrink-0 self-stretch my-auto max-w-full aspect-square w-[100px] md:w-auto"
-              />
-              <div className="flex flex-col justify-center self-stretch">
-                <div className="flex gap-5 justify-center items-center self-stretch mt-2 text-3xl font-semibold text-center text-white uppercase tracking-[5.12px] md:mt-0">
-                  <img
-                    loading="lazy"
-                    src={leftArrowImage}
-                    className="shrink-0 self-stretch my-auto border-white border-solid aspect-square border-[3px] stroke-[3px] stroke-white w-[33px]"
-                  />
-                  <div className="self-stretch">{planetName}</div>
-                  <img
-                    loading="lazy"
-                    src={rightArrowImage}
-                    className="shrink-0 self-stretch my-auto border-white border-solid aspect-square border-[3px] stroke-[3px] stroke-white w-[33px]"
-                  />
-                </div>
-                <div className="flex justify-center items-center p-1.5 mt-2 w-[47px] md:mt-0">
-                  <img loading="lazy" src={compassImage} className="w-full aspect-square" />
-                </div>
-              </div>
-            </div>
-            <img
-              loading="lazy"
-              src={logoImage}
-              className="shrink-0 self-stretch my-auto max-w-full aspect-square w-[100px] md:w-auto"
-            />
-          </div>
+  return (
+    <div className="flex justify-between items-center px-6 py-4">
+      <div className="flex items-center">
+        <img loading="lazy" src={logoImage} className="w-12 h-12 mr-2" />
+        <div className="flex flex-col items-center">
+          <img loading="lazy" src={leftArrowImage} className="w-6 h-6 mb-1" />
+          <div className="text-white">{planetName}</div>
+          <img loading="lazy" src={rightArrowImage} className="w-6 h-6 mt-1" />
         </div>
-      );
+        <img loading="lazy" src={compassImage} className="w-6 h-6 ml-auto" />
+      </div>
+    </div>
+  );
 };
 
 interface RoverSectionProps {
@@ -118,63 +97,22 @@ const RoverSection: React.FC<RoverSectionProps> = ({
   idleRoverImage,
 }) => {
   return (
-    <div className="relative self-center w-full max-w-[906px] mt-[1554px] max-md:mt-10 max-md:max-w-full">
-      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-        <div className="flex flex-col w-[26%] max-md:ml-0 max-md:w-full">
-          <div className="flex relative flex-col px-5 mt-1 text-xl font-semibold text-center text-white uppercase whitespace-nowrap tracking-[3.2px] max-md:mt-10">
-            <div>structure</div>
-            <div className="shrink-0 self-start mt-20 rounded-full shadow-2xl backdrop-blur-[5px] bg-cyan-300 bg-opacity-50 h-[45px] max-md:mt-10" />
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-between w-full px-6 mb-4">
+        {[1, 2, 3].map((index) => (
+          <div key={index} className="w-1/3 p-2">
+            <img loading="lazy" src={structureImage} className="w-full" />
           </div>
+        ))}
+      </div>
+      <div className="flex justify-between w-full px-6">
+        <div className="w-1/2">
+          <div className="text-center text-white">Active Rover</div>
+          <img loading="lazy" src={activeRoverImage} className="w-full" />
         </div>
-        <div className="flex flex-col ml-5 w-[74%] max-md:ml-0 max-md:w-full">
-          <div className="relative grow max-md:mt-10 max-md:max-w-full">
-            <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-              <div className="flex flex-col w-[68%] max-md:ml-0 max-md:w-full">
-                <div className="flex relative grow gap-4 items-start mt-16 max-md:mt-10">
-                  <div className="flex flex-col flex-1 px-5">
-                    <div className="justify-center self-center px-4 py-1 text-xl font-medium text-center text-white whitespace-nowrap rounded-xl shadow-sm bg-green-500 bg-opacity-40">
-                      Active
-                    </div>
-                    <div className="text-xl font-semibold text-center text-white uppercase tracking-[3.2px]">
-                      rover
-                    </div>
-                    <div className="flex flex-col justify-center px-10 py-0.5 max-md:px-5">
-                      <div className="flex flex-col items-start pt-5">
-                        <img
-                          loading="lazy"
-                          src={activeRoverImage}
-                          className="aspect-[1.18] w-[92px]"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col flex-1 px-5 mt-1.5">
-                    <div className="justify-center self-center px-4 py-1 text-xl font-medium text-center text-white whitespace-nowrap rounded-xl shadow-sm bg-red-600 bg-opacity-40">
-                      Idle
-                    </div>
-                    <div className="text-xl font-semibold text-center text-white uppercase tracking-[3.2px]">
-                      rover
-                    </div>
-                    <div className="flex flex-col justify-center px-10 py-0.5 max-md:px-5">
-                      <div className="flex flex-col items-start pt-5">
-                        <img
-                          loading="lazy"
-                          src={idleRoverImage}
-                          className="aspect-[1.18] w-[92px]"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col ml-5 w-[32%] max-md:ml-0 max-md:w-full">
-                <div className="flex relative flex-col px-5 text-xl font-semibold text-center text-white uppercase whitespace-nowrap tracking-[3.2px] max-md:mt-9">
-                  <div>structure</div>
-                  <div className="shrink-0 self-start mt-20 rounded-full shadow-2xl backdrop-blur-[5px] bg-cyan-300 bg-opacity-50 h-[45px] max-md:mt-10" />
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="w-1/2">
+          <div className="text-center text-white">Idle Rover</div>
+          <img loading="lazy" src={idleRoverImage} className="w-full" />
         </div>
       </div>
     </div>
@@ -188,30 +126,16 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ images, centerButtonImage }) => {
   return (
-    <div className="flex relative flex-col justify-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
-      <div className="flex flex-col justify-center py-5 w-full max-md:max-w-full">
-        <div className="flex flex-col justify-center w-full max-md:max-w-full">
-          <div className="flex justify-center items-center px-16 py-6 w-full max-md:px-5 max-md:max-w-full">
-            <div className="flex gap-5 justify-between max-md:flex-wrap">
-              {images.map((image, index) => (
-                <div
-                  key={index}
-                  className="flex overflow-hidden relative flex-col justify-center aspect-[1.08] w-[140px]"
-                >
-                  <img
-                    loading="lazy"
-                    src={image}
-                    className="object-cover absolute inset-0 size-full"
-                  />
-                  <div className="relative shrink-0 h-[130px]" />
-                </div>
-              ))}
-            </div>
+    <div className="flex justify-center">
+      <div className="flex flex-wrap w-full px-4">
+        {images.map((image, index) => (
+          <div key={index} className="w-1/3 p-2">
+            <img loading="lazy" src={image} className="w-full" />
           </div>
-        </div>
-        <div className="flex justify-center items-center self-center px-2.5 mt-10 bg-indigo-700 h-[108px] rounded-[100px] w-[108px]">
-          <img loading="lazy" src={centerButtonImage} className="w-full aspect-square" />
-        </div>
+        ))}
+      </div>
+      <div className="flex justify-center w-full">
+        <img loading="lazy" src={centerButtonImage} className="w-16 h-16 mb-8" />
       </div>
     </div>
   );
