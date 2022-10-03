f4286514011a9eb19f2fd571b375730c164febd3
Liam Arbuckle <liam@skinetics.tech>
2022-10-03 14:44:39 +0800

    🎛️📫 ↝ [SGV2-2 SGV2-8]: Removing archived

--- pages/tests/bento.tsx
+++ pages/tests/bento.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Layout from "../../components/_Core/Section/Layout";
 
 
 export default function BentoTestPage() {
 
     return (
         <div className="mt-8">
-            
+            <Layout>
+                g
+            </Layout>
         </div>
     );
 };
