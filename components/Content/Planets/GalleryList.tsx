b8fcbdc1879fb8f6a3ac1c01b7c55f5a03c9706a
Liam Arbuckle <liam@skinetics.tech>
2022-10-04 14:52:53 +0800

    🗳️⚖️ ↝ [SGV2-2 SGV2-8]: Removing gallery components

--- components/Content/Planets/GalleryList.tsx
+++ components/Content/Planets/GalleryList.tsx
@@ -8,90 +8,11 @@ interface Planet {
   id: string;
   content: string;
   avatar_url: string;
-}
+};
 
 interface Props {
   planet: Planet;
   position: { top: number; left: number };
-}
-
-export function PlanetGalleryCard({ planet, position }: Props) {
-  const { id, content, avatar_url } = planet;
-
-  return (
-    <Link legacyBehavior href={`/planets/${id}`}>
-      <a style={{ position: "absolute", top: position.top, left: position.left }}>
-        <img src={avatar_url} className="w-1/2" />
-      </a>
-    </Link>
-  );
-}
-
-export default function PlanetGallery() {
-  const supabase = useSupabaseClient();
-  const session = useSession();
-  const [planets, setPlanets] = useState<Planet[]>([]);
-
-  useEffect(() => {
-    getPlanets();
-  }, [session]);
-
-  const getPlanets = async (): Promise<void> => {
-    try {
-      let query = supabase
-        // .from("planetsss")
-        .from("basePlanets")
-        .select("*")
-        .order("created_at", { ascending: false })
-        .limit(200)
-        // .gte("id", 67)
-        // .lt("id", 75);
-
-      const { data, error } = await query;
-
-      if (data != null) {
-        setPlanets(data);
-        console.log(planets);
-      }
-
-      if (error) {
-        throw error;
-      }
-    } catch (error: any) {
-      alert(error.message);
-    }
-  };
-
-  const getRandomPosition = (): { top: number; left: number } => {
-    const top = Math.floor(Math.random() * window.innerHeight);
-    const left = Math.floor(Math.random() * window.innerWidth);
-    return { top, left };
-  };
-
-  const buttonStyle = {
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
-    border: "none",
-    color: "black",
-    transition: "background-color 0.3s ease",
-    cursor: "pointer",
-    marginRight: "8px",
-  };
-
-  const activeButtonStyle = {
-    backgroundColor: "rgba(255, 255, 255, 0.3)",
-  };
-
-  if (!session) {
-    return <Login />;
-  }
-
-  return (
-    <>
-      {planets.map((planet, index) => (
-        <PlanetGalleryCard key={planet.id} planet={planet} position={getRandomPosition()} />
-      ))}
-    </>
-  );
 };
 
 const PlanetGalleryWithSectors: React.FC = () => {
@@ -229,19 +150,6 @@ export const Garden: React.FC<GardenProps> = ({ onClose }) => {
           >
             Close
           </button> */}
-          <PlanetGalleryWithSectors />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const GardenBar: React.FC = () => {
-
-  return (
-    <div className={`fixed inset-x-0 bottom-0 flex justify-center transition-transform duration-300`}>
-      <div className="bg-cover bg-center w-full sm:max-w-screen-lg sm:w-full max-h-96vh overflow-y-auto shadow-lg relative rounded-t-3xl">
-        <div style={{ backgroundImage: `url('/garden.png')` }} className="bg-cover bg-center h-96vh flex items-center justify-center relative rounded-t-3xl">
           <PlanetGalleryWithSectors />
         </div>
       </div>
--- pages/tests/bento.tsx
+++ pages/tests/bento.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Layout from "../../components/_Core/Section/Layout";
+import { Garden } from "../../components/Content/Planets/GalleryList";
 
 
 export default function BentoTestPage() {
 
+    function test() {
+
+    }
+
     return (
         <div className="mt-8">
             <Layout>
-                g
+                <Garden onClose={test} />
             </Layout>
         </div>
     );
