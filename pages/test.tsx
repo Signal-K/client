e62a2848538aca90a905e0a79790b794d2b8dcec
Liam Arbuckle <liam@skinetics.tech>
2022-10-17 11:27:21 +1100

    🪻🪨 ↝ [SGV2-21 SGV2-20 SGV2-14 SGV2-24 SGV2-23 SGV2-7]: Just need to update the window to support rich props, classification media e.g. 💫☄️🌌🔭

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,12 @@
 import { AutomatonControlPanel } from "@/components/Gameplay/Inventory/Automation";
 import Layout from "@/components/Layout";
+import { RoverWindow } from "@/components/rover-window";
 
 export default function () {
     return (
         <Layout bg={false}>
             <div className="my-4 mb-6 px-3"><AutomatonControlPanel /></div>
+            <RoverWindow />
             <p>Whatever I want goes in here, lol</p>
         </Layout>
     );
