7315f94a415ccbe9361ca86e4ca86515fc44f4fd
Liam Arbuckle <liam@skinetics.tech>
2023-02-26 19:58:00 +1100

    💎🛹 ↝ New planet data added from table, found issues in #26

--- components/NavigationCard.tsx
+++ components/NavigationCard.tsx
@@ -36,7 +36,7 @@ export default function NavigationCard(){
           </svg>
           <span className="hidden md:block">Saved posts</span>
         </Link>
-        <Link href="/planets/cebdc7a2-d8af-45b3-b37f-80f328ff54d6" className={pathname === '/notifications' ? activeElementClasses : nonActiveElementClasses}> {/* For testing -> final should be /posts/notifications */}
+        <Link href="/planets/fdd401bf-3925-49ba-a155-4c3dba8dd93f" className={pathname === '/notifications' ? activeElementClasses : nonActiveElementClasses}> {/* For testing -> final should be /posts/notifications */}
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
           </svg>
