5ebd3f029c64dfb5913601f7a41ae0c83ad66846
Liam Arbuckle <liam@skinetics.tech>
2023-04-02 00:21:01 +1100

    📌🐟 ↝ Updating contracts dir for successful hardhat compilation, new governance contract to simplify testing & frontend components

--- pages/governance/index.tsx
+++ pages/governance/index.tsx
@@ -2,17 +2,31 @@ import React, { useEffect } from "react";
 import { Header } from "../../components/Governance/Header";
 import { Footer } from '../../components/Governance/Footer';
 import { Navbar } from "../../components/Governance/Navbar";
-import { useMetamaskState } from "../../components/Governance/Connections/ConnectWallet";
-import styles from '../../styles/governance/governance.module.css';
+//import { useMetamaskState } from "../../components/Governance/Connections/ConnectWallet";
+//import styles from '../../styles/governance/governance.module.css';
+import styles from '../../styles/Staking-P2E/planetInteraction.module.css';
+import { NextPage } from "next";
+import { useAddress } from "@thirdweb-dev/react";
 
-export default function GovernanceDAOIndex() {
-    const { isConnected, account, signer, connectToMetamask } = useMetamaskState();
+const GovernanceDAOIndex: NextPage = () => {
+    //const { isConnected, account, signer, connectToMetamask } = useMetamaskState();
+    const address = useAddress();
+    const contractAddress = ''; // Set this to deployed "planetProposal.sol" contract
 
     return (
         <>
-            <Header connectToMetamask={connectToMetamask} isConnected={isConnected} account={account} signer={signer} />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <div className={styles.card}>
+
+                    </div>
+                </main>
+            </div>
+            {/*<Header connectToMetamask={connectToMetamask} isConnected={isConnected} account={account} signer={signer} />
             <Navbar signer={signer} />
-            <Footer />
+            <Footer />*/}
         </>
-    )
-}
+    );
+};
+
+export default GovernanceDAOIndex;
