431637114c6491858b7f011bed759b848d3f44e4
Liam Arbuckle <liam@skinetics.tech>
2023-04-12 20:44:29 +0800

    🐲🩹 ↝ Now building again from wb3-16-fix-build-errors-originating-from-new on linear

--- pages/posts/post/[id].tsx
+++ pages/posts/post/[id].tsx
@@ -2,5 +2,8 @@ import React from "react";
 import PostPage from "../post";
 
 export default function Post () {
-    return <PostPage />;
+    //return <PostPage />;
+    return (
+        <div>Stub, PostPage</div>
+    )
 }
