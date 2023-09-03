52844f4d581ea2e5d2db721da012a470c167386e
Liam Arbuckle <liam@skinetics.tech>
2023-09-03 15:30:06 +1000

    💼🥑 ↝ Updating more planet build stats

--- components/Gameplay/Unity/Build/LOD-Mesh.tsx
+++ components/Gameplay/Unity/Build/LOD-Mesh.tsx
@@ -22,25 +22,33 @@ export default function UnityBuildSupabaseMesh ({ planetName }) {
         sendMessage("5", "UpdatePlanetName", planetName);//session?.user?.id);
     };
 
+    const handleUpdatePlanetSpeed = () => {
+        sendMessage("Planet", "UpdatePlanetTurnSpeed", 75);
+    };
+
+    const handleGeneratePlanet = () => {
+        sendMessage("Planet", "GeneratePlanet");
+    };
+
     // State to read data FROM unity scene
     const [receivedPlanetName, setReceivedPlanetName] = useState("");
-    useEffect(() => {
-        // Add an event listener to listen for messages from Unity
-        const handleUnityMessage = (message) => {
-            if (message.name === "UnityToReactMessage" && message.data === "PlanetNameUpdated") {
-                // Received planet name from Unity, update the state
-                setReceivedPlanetName(message.value);
-            }
-        };
+    // useEffect(() => {
+    //     // Add an event listener to listen for messages from Unity
+    //     const handleUnityMessage = (message) => {
+    //         if (message.name === "UnityToReactMessage" && message.data === "PlanetNameUpdated") {
+    //             // Received planet name from Unity, update the state
+    //             setReceivedPlanetName(message.value);
+    //         }
+    //     };
 
-        // Register the event listener
-        addEventListener("message", handleUnityMessage);
+    //     // Register the event listener
+    //     addEventListener("message", handleUnityMessage);
 
-        // Cleanup by removing the event listener when the component unmounts
-        return () => {
-            removeEventListener("message", handleUnityMessage);
-        };
-    }, []);
+    //     // Cleanup by removing the event listener when the component unmounts
+    //     return () => {
+    //         removeEventListener("message", handleUnityMessage);
+    //     };
+    // }, []);
 
     return (
         <Fragment>
@@ -52,6 +60,8 @@ export default function UnityBuildSupabaseMesh ({ planetName }) {
                 // onChange={handlePlanetNameChange}
             />
             <button onClick={handleSendPlanetName}>Send Planet Name</button>
+            <button onClick={handleUpdatePlanetSpeed}>Increase rotation</button>
+            <button onClick={handleGeneratePlanet}>Generate planet</button>
         </Fragment>
     );
 }
