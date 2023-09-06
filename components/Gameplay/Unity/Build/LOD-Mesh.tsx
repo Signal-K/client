8513cd2c25775d6fd4f76fcaaa3910a63adbb72c
Liam Arbuckle <liam@skinetics.tech>
2023-09-06 15:58:02 +1000

    🐒🥃 ↝ Some minor updates to the gallery page

--- components/Gameplay/Unity/Build/LOD-Mesh.tsx
+++ components/Gameplay/Unity/Build/LOD-Mesh.tsx
@@ -3,16 +3,14 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function UnityBuildSupabaseMesh ({ planetName }) {
+    const session = useSession();
+
     const { unityProvider, sendMessage, addEventListener, removeEventListener } = useUnityContext({
         loaderUrl: "/assets/Mars/Mesh/Rocky_Earth-base.Mesh.loader.js",
         dataUrl: "/assets/Mars/Mesh/Rocky_Earth-base.Mesh.data",
         frameworkUrl: "/assets/Mars/Mesh/Rocky_Earth-base.Mesh.framework.js",
         codeUrl: "/assets/Mars/Mesh/Rocky_Earth-base.Mesh.wasm",
     });
-
-    // const [planetName, setPlanetName] = useState("");
-    
-    const session = useSession();
  
     // const handlePlanetNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     //     setPlanetName(event.target.value);
@@ -30,38 +28,39 @@ export default function UnityBuildSupabaseMesh ({ planetName }) {
         sendMessage("Planet", "GeneratePlanet");
     };
 
-    // State to read data FROM unity scene
-    const [receivedPlanetName, setReceivedPlanetName] = useState("");
-    // useEffect(() => {
-    //     // Add an event listener to listen for messages from Unity
-    //     const handleUnityMessage = (message) => {
-    //         if (message.name === "UnityToReactMessage" && message.data === "PlanetNameUpdated") {
-    //             // Received planet name from Unity, update the state
-    //             setReceivedPlanetName(message.value);
-    //         }
-    //     };
-
-    //     // Register the event listener
-    //     addEventListener("message", handleUnityMessage);
-
-    //     // Cleanup by removing the event listener when the component unmounts
-    //     return () => {
-    //         removeEventListener("message", handleUnityMessage);
-    //     };
-    // }, []);
-
     return (
         <Fragment>
             <Unity unityProvider={unityProvider} style={{ width: "100%", height: "500px" }}/>
-            <input
+            {/* <input
                 type="text"
                 placeholder="Enter planet name"
                 value={planetName}
                 // onChange={handlePlanetNameChange}
-            />
-            <button onClick={handleSendPlanetName}>Send Planet Name</button>
-            <button onClick={handleUpdatePlanetSpeed}>Increase rotation</button>
-            <button onClick={handleGeneratePlanet}>Generate planet</button>
+            /> */}
+            <center><button className="btn glass mr-5 mt-1.5" onClick={handleSendPlanetName}>Send Planet Name</button>
+            <button className="btn glass mr-5 mt-1.5" onClick={handleUpdatePlanetSpeed}>Increase rotation</button>
+            <button className="btn glass mr-5 mt-1.5" onClick={handleGeneratePlanet}>Generate planet</button></center>
         </Fragment>
     );
 }
+
+
+/* State to read data FROM unity scene
+const [receivedPlanetName, setReceivedPlanetName] = useState("");
+// useEffect(() => {
+//     // Add an event listener to listen for messages from Unity
+//     const handleUnityMessage = (message) => {
+//         if (message.name === "UnityToReactMessage" && message.data === "PlanetNameUpdated") {
+//             // Received planet name from Unity, update the state
+//             setReceivedPlanetName(message.value);
+//         }
+//     };
+
+//     // Register the event listener
+//     addEventListener("message", handleUnityMessage);
+
+//     // Cleanup by removing the event listener when the component unmounts
+//     return () => {
+//         removeEventListener("message", handleUnityMessage);
+//     };
+// }, []);*/
