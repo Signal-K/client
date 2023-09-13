3c2f449bd35b40149db0b23cb3fa1ba32f93f1ee
Liam Arbuckle <liam@skinetics.tech>
2023-09-13 13:12:50 +1000

    🍓💍 ↝ Fix some logs/api ts. Not feeling so roger-roger

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -1,11 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
+
+interface AuthRequestBody {
+  auth: {
+    username: string;
+    password: string;
+  };
+}
+
+interface TokenResponse {
+  // Define the structure of the token response here
+  // For example, if it's an object with specific fields, define them here
+  // For this example, let's assume it's an object with a 'token' field
+  token: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<TokenResponse | { error: string }>
 ) {
   try {
-    const response = await axios.post(
+    const response = await axios.post<TokenResponse>(
       "https://appeears.earthdatacloud.nasa.gov/api/login",
       {
         auth: {
@@ -14,7 +29,10 @@ export default async function handler(
         },
       }
     );
-    const tokenResponse = await response.json();
+
+    // Assuming the token is a string in the response
+    const tokenResponse: TokenResponse = response.data;
+
     res.status(200).json(tokenResponse);
   } catch (error) {
     res.status(500).json({ error: error.message });
