c7f43f53036d03496788e0b15942702b6c64daa8
Liam Arbuckle <liam@skinetics.tech>
2023-09-16 07:55:18 +1000

    🪵🫅🏻 ↝ Flux variability route

--- components/Gameplay/Planets/Data/PlanetDataSend.tsx
+++ components/Gameplay/Planets/Data/PlanetDataSend.tsx
@@ -1,31 +1,43 @@
 import React from "react";
 import axios from "axios";
-
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Login from "../../../../pages/login";
+import UnityBuildSupabaseMesh from "../../Unity/Build/LOD-Mesh";
 
 export default function SendPlanetsssDataToFlask () {
+    const supabase = useSupabaseClient(); // Draw planet data (tests/planet.tsx) via Supabase, pass in as props/arguments
+    const session = useSession();
 
-// Define the data to send
-const dataToSend = {
-    planetName: "Earth",
-    userName: "John Doe",
-    planetData: "Some data about Earth"
-  };
+    const dataToSend = { // Define the data to send
+        planetName: "Earth",
+        userName: "John Doe",
+        planetData: "Some data about Earth"
+    };
+
+    const ticId = {
+        ticId: "KIC 10593626", // Update this to draw from Supabase like in the LodRocky component/mesh
+    }
   
-  // Send a POST request to the Flask app
-  fetch('/receive_data', { // http://localhost:5000
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    
-    body: JSON.stringify(dataToSend)
-  })
+    // Send a POST request to the Flask app
+    fetch('http://localhost:5000/flux_variability', { // fetch('/receive_data', { // http://localhost:5000
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(dataToSend)
+    })
     .then(response => response.json())
     .then(data => {
-      console.log(data.message); // Should print "Data received successfully"
+        console.log(data.message); // Should print "Data received successfully"
     })
     .catch(error => {
-      console.error(error);
+        console.error(error);
     });
   
-}
+    if (!session) {
+        return (
+            <Login />
+        );
+    };
+};
