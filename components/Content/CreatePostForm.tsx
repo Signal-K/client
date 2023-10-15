000c3c2aef059304bc5935465424b67eb6ce76e0
Liam Arbuckle <liam@skinetics.tech>
2023-10-16 08:45:51 +1100

    🐙🍹 ↝ Working on ability to create posts

---
+++ components/Content/CreatePostForm.tsx
@@ -0,0 +1,3 @@
+import { Button } from "../ui/Button";
+// import { Card }
+// Look into methods to engage resource gathering with the lightcurves, make it a scrollable feed with planet content, show how the curves lead into the rover.
