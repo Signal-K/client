48866c6f025d739fb122889ef817e472548d4951
Liam Arbuckle <liam@skinetics.tech>
2023-10-23 17:20:04 +0800

    🧸🧂 ↝ looking into backend schema for creation of base-base planet instances

--- components/Section/Sidebar.tsx
+++ components/Section/Sidebar.tsx
@@ -257,6 +257,14 @@ export function DesktopSidebar () {
                 <span className="ml-2 text-sm tracking-wide truncate">Gather</span>
               </a>
             </li>
+            <li>
+              <a href="/onboarding/select" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                <span className="inline-flex justify-center items-center ml-4">
+                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+                </span>
+                <span className="ml-2 text-sm tracking-wide truncate">Pick your planet</span>
+              </a>
+            </li>
       </ul>
   </div>
   </div>
