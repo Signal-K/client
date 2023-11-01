7fb37dbc99e9b482574f410ec954a4b99d91c434
Liam Arbuckle <liam@skinetics.tech>
2023-11-01 21:07:27 +1100

    🇬🇭👨🏻‍🎨 ↝ New layout for planet

--- components/Section/Layout.tsx
+++ components/Section/Layout.tsx
@@ -26,6 +26,41 @@ const Layout: React.FC<DashboardLayoutProps> = ({ children }) => {
 
   return (
     <>
+        <main className="h-max pb-10 grow pt-6">
+          {/* <Navbar /> */}
+          {children}
+        </main>
+      {isMobile && (
+        <div className="md:hidden overflow-y-auto h-screen p-4">
+          <main className="h-max pb-10 grow">{children}</main>
+          <Bottombar />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Layout;
+
+export const LayoutWithSidebar: React.FC<DashboardLayoutProps> = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check if window is defined before accessing it
+    if (typeof window !== "undefined") {
+      const checkIsMobile = () => {
+        setIsMobile(window.innerWidth <= 768);
+      };
+      checkIsMobile();
+      window.addEventListener("resize", checkIsMobile);
+      return () => {
+        window.removeEventListener("resize", checkIsMobile);
+      };
+    }
+  }, []);
+
+  return (
+    <>
       <div className="hidden md:flex relative items-start">
         <DesktopSidebar />
         <main className="h-max pb-10 grow ml-64 pt-6">
@@ -42,8 +77,6 @@ const Layout: React.FC<DashboardLayoutProps> = ({ children }) => {
     </>
   );
 };
-
-export default Layout;
 
 export const LayoutNoNav: React.FC<DashboardLayoutProps> = ({ children }) => {
   const [isMobile, setIsMobile] = useState(false);
