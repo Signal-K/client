2d75c1ad15fa0f80b8c8a67ae90c9eafd58ef6be
Liam Arbuckle <liam@skinetics.tech>
2023-12-01 12:27:56 +1100

    👝🚊 ↝ Making some adjustments to the sector schema

--- components/Content/Planets/Base/IndividualBasePlanetSector.tsx
+++ components/Content/Planets/Base/IndividualBasePlanetSector.tsx
@@ -76,61 +76,55 @@ export default function BasePlanetSector({ sectorid }: { sectorid: string }) {
     // I think that sectors will have posts attributed to them via contentPLANETSECTORS -> following the IndividualBasePlanet.tsx fetch functions
 
     return (
-        <div className="flex-col justify-center p-5">
-            <style jsx global> 
+        <div className="flex-col justify-center p-5 mt-[-80px]">
+          <style jsx global>
             {`
-          body {
-            background: url('https://images.unsplash.com/photo-1522441815192-d9f04eb0615c?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center/cover;
-            );
-          }
-          @media only screen and (max-width: 1000px) {
-            body {
-              background: url('/void.png') center/cover;
-            }
-          }
-          @media only screen and (max-width: 767px) {
-              .planet-heading {
-                color: white;
-                font-size: 24px;
-                text-align: center;
-                margin-bottom: 10px;
+              body {
+                background: url('https://images.unsplash.com/photo-1545243424-0ce743321e11?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center/cover;
               }
-        `}
-            </style>
-            <div className="h-screen flex flex-col items-center justify-center relative">
-                <h1 className="text-center text-slate-300 text-opacity-100 font-['Inter'] tracking-[3.48px] mt-2 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-gray-400">
-                    Planet {planetData?.content}, Sector {id}
-                </h1>
-                <img
-                    src={coverUrl}
-                    alt="Rover sector image"
-                    className={`w-4/12 h-4/12 sm:w-4/11 sm:h-4/11 object-contain z-20`}
-                    style={{ zIndex: 20 }}
-                />
-                <div className="flex items-start gap-8 py-10">
+              @media only screen and (max-width: 1000px) {
+                body {
+                  background: url('/void.png') center/cover;
+                }
+              }
+              @media only screen and (max-width: 767px) {
+                .planet-heading {
+                  color: white;
+                  font-size: 24px;
+                  text-align: center;
+                  margin-bottom: 10px;
+                }
+              `}
+          </style>
+          <div className="h-screen flex flex-col items-center justify-center relative">
+            <h1 className="text-center text-slate-300 text-opacity-100 font-['Inter'] tracking-[3.48px] mt-[-50px] mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-gray-400">
+              Planet {planetData?.content}, Sector {id}
+            </h1>
+            <img
+              src={coverUrl}
+              alt="Rover sector image"
+              className={`w-3/12 h-3/12 sm:w-4/11 sm:h-4/11 object-contain z-20 p-10 mb-20`}
+              style={{ zIndex: 20 }}
+            />
+            <div className="flex items-start gap-8 py-10 mt-20">
   <div className="flex flex-col items-center justify-start gap-4">
-    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] tracking-[3.48px]">Mass</div>
-    <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">mE</div>
+    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] tracking-[3.48px]">Planet</div>
+    <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">{planetData?.content}</div>
   </div>
 
   <div className="flex flex-col items-center justify-start gap-4">
-    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">Semi-Major Axis</div>
-    <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">AU</div>
+    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">Mineral deposit</div>
+    <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">{deposit}</div>
   </div>
 
   <div className="flex flex-col items-center justify-start gap-4">
-    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">Anomaly type</div>
+    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">Exploration status</div>
     <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">?</div>
   </div>
 
   <div className="flex flex-col items-center justify-start gap-4">
     <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">Eq. Temperature</div>
     <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">°C</div>
-  </div>
-
-  <div className="flex flex-col items-center justify-start gap-4">
-    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] uppercase tracking-[3.48px]">TIC ID</div>
-    <div className="text-center text-white text-opacity-90 text-[27.17px] font-medium font-['Inter']">T</div>
   </div>
 </div>
             </div>
