416056c16b0d66684b636b6c29c6cf5244f51bc2
Liam Arbuckle <liam@skinetics.tech>
2023-12-02 15:25:36 +0800

    👚🌚 ↝ [ FCDB-24 ] Login modal now working

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router'; // Import the useRouter hook
 import Layout from '../../components/Section/Layout';
+import LoginPage from '../../components/Authentication/LoginModal';
 
 const Login = () => {
     const session = useSession();
@@ -19,12 +20,56 @@ const Login = () => {
     return (
         <div className='container' style={{ padding: '50px 0 100px 0' }}>
             {!session ? (
-                <div className='w-80%'><Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='dark' /></div>
+                // <div className='w-80%'><Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='dark' /></div>
+                <div><LoginPage /></div>
             ) : (
                 <Layout>Logged in</Layout>
             )}
         </div>
     );
-}
+};
 
 export default Login;
+
+/*
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { LoginForm } from '@wasp/auth/forms/Login';
+import { AuthWrapper } from './authWrapper';
+import useAuth from '@wasp/auth/useAuth';
+
+export default function Login() {
+  const history = useHistory();
+
+  const { data: user } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [user, history]);
+
+  return (
+    <AuthWrapper>
+      <LoginForm />
+      <br />
+      <span className='text-sm font-medium text-gray-900 dark:text-gray-900'>
+        Don't have an account yet?{' '}
+        <Link to='/signup' className='underline'>
+          go to signup
+        </Link>
+        .
+      </span>
+      <br />
+      <span className='text-sm font-medium text-gray-900'>
+        Forgot your password?{' '}
+        <Link to='/request-password-reset' className='underline'>
+          reset it
+        </Link>
+        .
+      </span>
+    </AuthWrapper>
+  );
+}
+*/
