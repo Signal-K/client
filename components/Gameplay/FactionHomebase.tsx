594d9106e815e8b1102dbd13d281de967da6d4eb
Liam Arbuckle <liam@skinetics.tech>
2023-12-04 15:43:01 +1100

    🤹🏻‍♂️🔖 ↝ [ FCDB-13 ]: Faction data, based on user's faction and planet, is now showing && git push

--- components/Gameplay/FactionHomebase.tsx
+++ components/Gameplay/FactionHomebase.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card from "../Card";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
-export default function HomebaseSelector(factionId) {
+export default function HomebaseSelector({ factionId }) {
     const supabase = useSupabaseClient();
     const session = useSession();
 
@@ -13,57 +13,57 @@ export default function HomebaseSelector(factionId) {
         if (!session) {
             return null;
         }
-    
+
         try {
             const { data, error } = await supabase
                 .from("basePlanets")
                 .select("*")
-                .eq('id', factionId)
+                .eq('Faction', factionId)
                 .single();
-    
+
             if (data) {
                 setPlanetData(data);
             }
-    
+
             if (error) {
                 throw error;
             }
         } catch (error) {
             console.error(error.message);
         }
-    };    
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             if (session && factionId) {
                 await getPlanetData();
-            };
+            }
         };
 
         fetchData();
-    }, [factionId]);
+    }, [session, factionId]);
 
-    const { id, created_at, content, coverUrl } = planetData || {};
+    const { content, coverUrl } = planetData || {};
 
     return (
         <>
             <Card noPadding={false}>
-                <div 
+                <div
                     className="flex-col justify-center mt-[-80px] bg-cover bg-center rounded-15"
                     style={{
-                        backgroundImage:
-                          'url("https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?q=80&w=2372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+                        // backgroundImage: `url(${coverUrl})`,
+                        backgroundImage: `url('https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?q=80&w=2372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
                     }}
                 >
                     <div className="h-[80vh] flex flex-col items-center justify-center relative">
                         <h1 className="text-center text-slate-300 text-opacity-100 font-['Inter'] tracking-[3.48px] mt-[-50px] mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-gray-400">
-                            Your faction
+                            {content}
                         </h1>
                     </div>
                 </div>
             </Card>
             <div className="h-grow px-10">
-                <h1 className="text-center text-slate-300 text-opacity-100 font-['Inter'] tracking-[3.48px] mt-[-50px] mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-gray-400">{content}</h1>
+                
             </div>
         </>
     );
--- pages/factions/index.tsx
+++ pages/factions/index.tsx
@@ -4,18 +4,67 @@
 // Row in `basePlanets` `Faction`': Will later be a foreign key to the (currently non-existent) `factions` table. Will be labelled 1-6, for simplicity with pulling ids in the client code. Naming schema & other variables will be set manually in client repo UNTIL new table is created
 // Add in a toggle to view other factions once you've "earnt their trust", right now we don't need or want people to view others' homebases.
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Section/Layout";
 import HomebaseSelector from "../../components/Gameplay/FactionHomebase";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 export default function Homebase() {
-    const factionIdBase: number = 1 as number; // Will add a column to the `profiles` table imminently.
+    const supabase = useSupabaseClient();
+    const session = useSession();
+
+    // const [userFaction, setUserFaction] = useState<number>();
+    const [userData, setUserData] = useState(null);
 
     // Get user data from here, determine their faction
+    const getUserFaction = async () => {
+        if (!session) {
+            return null;
+        };
+
+        try {
+            const {
+                data, error 
+            } = await supabase
+                .from('profiles')
+                .select('*')
+                .eq('id', session?.user?.id)
+                .single();
+            
+            if (data) {
+                setUserData(data);
+            };
+
+            if (error) {
+                throw error;
+            };
+        } catch (error) {
+            console.error(error.message);
+        };
+    };
+
+    const { level, location, factions } = userData || {};
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (session) {
+                await getUserFaction();
+            };
+        };
+
+        fetchData();
+    }, [session]);
+
+    if (!userData) {
+        return (
+            <div>Loading...</div>
+        );
+    };
 
     return (
         <Layout>
-            <HomebaseSelector factionId={factionIdBase} />
+            <HomebaseSelector factionId={userData.factions} />
+            
         </Layout>
     );
 };
