46ca28286d10cdb90013daceae1f098d7bb201cb
Liam Arbuckle <liam@skinetics.tech>
2023-12-06 00:42:42 +1100

    🍋🧊 ↝ [ CPW-8 ]: Improved the styling on the (sectorS) route. Not important for current sprint so going to leave it there

--- components/Content/Planets/Base/BasePlanetAllSectors.tsx
+++ components/Content/Planets/Base/BasePlanetAllSectors.tsx
@@ -57,15 +57,69 @@ export default function BasePlanetSectors({ planetId }: { planetId: string }) {
         return <div>Loading...</div>;
     }
 
+    if (!session) {
     return (
         <>
+            <Card noPadding={false}>
+            <div
+          className="flex-col justify-center mt-[-80px] bg-cover bg-center rounded-15"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1578309756042-b445687e326c?q=80&w=2980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+          }}
+        ></div>
+            </Card>
             <div className="">
-                {Object.keys(sectorsData).map((key) => (
-                    <div key={key}>
-                        <strong>{key}:</strong> {JSON.stringify(sectorsData[key])}
-                    </div>
-                ))}
-            </div>
+                    {Object.keys(sectorsData).map((key) => (
+                        <div key={key}>
+                            <strong>{key}:</strong> {JSON.stringify(sectorsData[key])}
+                        </div>
+                    ))}
+                </div>
         </>
     );
+                    }
+
+                    return (
+                        <>
+                          <Card noPadding={false}>
+                            <div
+                              className="flex-col justify-center mt-[-80px] bg-cover bg-center rounded-15"
+                              style={{
+                                backgroundImage:
+                                'url("https://images.unsplash.com/photo-1578309756042-b445687e326c?q=80&w=2980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+                              }}
+                            >
+                              <div className="h-[80vh] flex flex-col items-center justify-center relative">
+                                <h1 className="text-center text-slate-300 text-opacity-100 font-['Inter'] tracking-[3.48px] mt-[-50px] mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-gray-400">
+                                  Test
+                                </h1>
+                                <div className="w-full flex items-center justify-center">
+                                </div>
+                                <div className="flex items-start gap-8 mt-20">
+                                  <div className="flex flex-col items-center justify-start gap-4">
+                                    <div className="text-center text-slate-300 text-opacity-70 text-[21.73px] font-medium font-['Inter'] tracking-[3.48px]">
+                                      Planet {planetId} sector list
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              {/* {deposit && typeof deposit === "string" ? (
+                                    <div>{deposit}</div>
+                                ) : (
+                                    <div>{JSON.stringify(deposit)}</div>
+                                )} */}
+                            </div>
+                          </Card>
+                          <div>
+                            <Card noPadding={false}>
+                            {Object.keys(sectorsData).map((key) => (
+                        <div key={key}>
+                            <strong>{key}:</strong> {JSON.stringify(sectorsData[key])}
+                        </div>
+                    ))}
+                            </Card>
+                          </div>
+                        </>
+                      );
 };
