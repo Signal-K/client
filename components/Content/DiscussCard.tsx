12bffce27d0007896bdbb21159c282004e160d5e
Liam Arbuckle <liam@skinetics.tech>
2023-12-07 16:48:08 +1100

    🔥🥅 ↝ [ CPW-8 ]: Setting up a new kind of postcard, just for rover data, in a grid

--- components/Content/DiscussCard.tsx
+++ components/Content/DiscussCard.tsx
@@ -138,3 +138,15 @@ const CardForum: React.FC<TProps> = ({
 };
 
 export default CardForum;
+
+export function RoverContentCard() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-md">
+        <div className="flex items-center justify-between mb-4">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
