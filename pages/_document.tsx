f386bfa1732382e1626c58f13f4630ce5846c4a4
Liam Arbuckle <liam@skinetics.tech>
2024-01-31 20:58:32 +0800

    🛟🤾🏻 ↝ Now a PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,9 +23,9 @@ class _Document extends Document {
           {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
           {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
 
-          <link rel='apple-touch-icon' sizes='180x180' href='https://cdn.cloud.scenario.com/assets/VFF4bSNWSqqsJFrcS8-X9A?p=100&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9jZG4uY2xvdWQuc2NlbmFyaW8uY29tL2Fzc2V0cy9WRkY0YlNOV1NxcXNKRnJjUzgtWDlBP3A9MTAwKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcwOTg1NTk5OX19fV19&Key-Pair-Id=K36FIAB9LE2OLR&Signature=FQKQ-UEuzxBmJ15OI8kdjqRQf~3Eb40E-whumwnM4bXjfibv3YQ9HJWQNM75Z12OBiGfEcMd4O3izAt09oHaGI7d8QZ3pHPHCVqqDiU5ZwXGULQPGNANUd0fpSkotEjNAGTCO-Oq4mmkHLMAkmAf2PRqz5Sdh-rPVYi3n694KnvD7YB8kSySI5O40c7P8GTA5Gp0Y59FTWwpFxXH1NVZFgRd33WqdUTwwqrj-YjsmHem2r8LF78bqTFhrGTKU906p8sSVa~V3ma~PLvlJI2l4CqjbUhdc1lyv85hGSq1qVZrs-SkTdfeU7C7X-g3ww5NNWJ2XV0IHLM7zktAYP3ICQ__' />
+          <link rel='apple-touch-icon' sizes='180x180' href='https://i.ibb.co/qrSP4gq/Removebg.png' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='https://cdn.cloud.scenario.com/assets/VFF4bSNWSqqsJFrcS8-X9A?p=100&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9jZG4uY2xvdWQuc2NlbmFyaW8uY29tL2Fzc2V0cy9WRkY0YlNOV1NxcXNKRnJjUzgtWDlBP3A9MTAwKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcwOTg1NTk5OX19fV19&Key-Pair-Id=K36FIAB9LE2OLR&Signature=FQKQ-UEuzxBmJ15OI8kdjqRQf~3Eb40E-whumwnM4bXjfibv3YQ9HJWQNM75Z12OBiGfEcMd4O3izAt09oHaGI7d8QZ3pHPHCVqqDiU5ZwXGULQPGNANUd0fpSkotEjNAGTCO-Oq4mmkHLMAkmAf2PRqz5Sdh-rPVYi3n694KnvD7YB8kSySI5O40c7P8GTA5Gp0Y59FTWwpFxXH1NVZFgRd33WqdUTwwqrj-YjsmHem2r8LF78bqTFhrGTKU906p8sSVa~V3ma~PLvlJI2l4CqjbUhdc1lyv85hGSq1qVZrs-SkTdfeU7C7X-g3ww5NNWJ2XV0IHLM7zktAYP3ICQ__' />
+          <link rel='shortcut icon' href='https://i.ibb.co/qrSP4gq/Removebg.png' />
           <style>{`
             html, body, #__next {
               height: 100%;
