a98cd35aed1841f89c1f2eb63c667c13209552cd
Liam Arbuckle <liam@skinetics.tech>
2024-02-04 01:14:04 +0800

    👩🏻‍🦽🛀🏼 ↝ Fixing build errors

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-	dest: 'public',
-	register: true,
-	skipWaiting: true,
-  })
-  
-  module.exports = withPWA({
-	// next.js config
-	reactStrictMode: false,
-	swcMinify: false,
-	// images: {
-	// 	unoptimised: true,
-	// },
-  });
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true
+	}
+};
+
+module.exports = nextConfig;
