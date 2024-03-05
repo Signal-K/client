29101b417d8e56dc7e3ff644048265c62911ef36
Liam Arbuckle <liam@skinetics.tech>
2024-03-05 13:56:06 +0800

    🍞⚓️ ↝ Some updates, planning/briefing [SGV2-10 SGV2-2 SGV2-1 SGV2-7]

--- components/Gameplay/onboarding.tsx
+++ components/Gameplay/onboarding.tsx
@@ -91,7 +91,7 @@ function getAccordionTitle(index) {
 function getAccordionContent(index) {
   switch (index) {
     case 0:
-      return <p>Content for Section 1</p>;
+      return <p>Content for Section 1. Ringed layout</p>;
     case 1:
       return <p><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
       <circle cx="100" cy="100" r="100" fill="#aaa" />
