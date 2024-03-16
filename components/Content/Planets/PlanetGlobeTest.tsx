55e929ba9c6f52b55d0fe25b76a4ab2a83f1c28a
Liam Arbuckle <liam@skinetics.tech>
2024-03-16 12:13:11 +0800

    ️👩‍👩‍👧‍👧🍒 ↝ [SGV2-7 GP-16]: Having a go at creating new landmasses

--- components/Content/Planets/PlanetGlobeTest.tsx
+++ components/Content/Planets/PlanetGlobeTest.tsx
@@ -28,7 +28,7 @@ export function GlobeDemo() {
     maxRings: 9,
     initialPosition: { lat: 22.3193, lng: 114.1694 },
     autoRotate: true,
-    autoRotateSpeed: 0.23,
+    autoRotateSpeed: 0.03,
   };
   const colors = ["#06b6d4", "06b6d4", "#6366f1"];
   const sampleArcs = [
@@ -412,11 +412,9 @@ export function GlobeDemo() {
           className="div"
         >
           <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
-            We sell soap worldwide
+            
           </h2>
           <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-            This globe is interactive and customizable. Have fun with it, and
-            don&apos;t forget to share it. :)
           </p>
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent z-40" />
