05767478bf95ad559d2a49b610ddf31de80bb652
Gizmotronn <liam@skinetics.tech>
2024-10-19 22:10:52 +0800

    👓✍🏻 ↝ [SSG-38]: Fixing dependency ci/cd error in previous commit

--- app/starnet/profile/page.tsx
+++ app/starnet/profile/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import StarnetLayout from "@/components/Layout/Starnet";
 import MissionPathway from "@/components/Missions/Pathway";
-import ProfileCardModal from "@/components/Profile/form";
+import ProfileCardModal from "@/components/profile/form";
 
 export default function TestPage() {
     return (
--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import StarnetLayout from "@/components/Layout/Starnet";
 import { ExoplanetTransitHunter } from "@/components/Projects/Telescopes/ExoplanetC23";
 import MissionPathway from "@/components/Missions/Pathway";
-import ProfileCardModal from "@/components/Profile/form";
+import ProfileCardModal from "@/components/profile/form";
 
 export default function TestPage() {
     return (
